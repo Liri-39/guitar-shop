e02679efaafbdd311d167c21b4f342009edc05af
Liri-39 <lirina74@gmail.com>
2021-10-25 20:10:26 +0200

    fix10

--- src/components/product-info/product-info.jsx
+++ src/components/product-info/product-info.jsx
@@ -5,7 +5,7 @@ import {productPropTypes} from "../../prop-types";
 
 const ProductInfo = ({product}) => {
     return <div className="modal__product-info product-info">
-        <p className="product-info__title">{product.type} {product.name}</p>
+        <p className="product-info__title">{GuitarName[product.type]} {product.name}</p>
         <p className="product-info__article">Артикул: {product.code}</p>
         <p className="product-info__info">{GuitarName[product.type]}, {product.strings} струнная</p>
         <p className="product-info__price">Цена: {priceFormat(product.price)} ₽</p>
