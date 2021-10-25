b5bb86735214b36309e8b8b247ee25dc637ed364
Liri-39 <lirina74@gmail.com>
2021-10-25 17:57:20 +0200

    fix7

--- src/components/breadcrumbs/breadcrumbs.jsx
+++ src/components/breadcrumbs/breadcrumbs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {AppRoute} from "../../const";
 
 const Breadcrumbs = () => {
@@ -7,8 +7,13 @@ const Breadcrumbs = () => {
 
     return <ul className="breadcrumbs">
         <li className="breadcrumbs__item">Главная</li>
-        <li className="breadcrumbs__item">Каталог</li>
-        {AppRoute.CART === location.pathname && <li className="breadcrumbs__item">Оформляем</li>}
+        {AppRoute.CART === location.pathname ?
+            <>
+            <li className="breadcrumbs__item"><Link to={AppRoute.CATALOG}>Каталог</Link></li>
+            <li className="breadcrumbs__item">Оформляем</li>
+            </>:
+            <li className="breadcrumbs__item">Каталог</li>
+        }
     </ul>
 }
 
