4d5e90ca9d0e1f18b52db9e2614b8c1ec947c1e0
Liri-39 <lirina74@gmail.com>
2021-10-28 15:09:48 +0200

    fix20

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {AppRoute} from "../../const";
 
 const Nav = () => {
+    const location = useLocation();
+
     return <nav className="header__nav nav">
         <ul className="nav__list">
-            <li className="nav__item"><Link to={AppRoute.CATALOG}  className="nav__link">Каталог</Link></li>
+            {AppRoute.CATALOG === location.pathname ?
+                <li className="nav__item"><a className="nav__link">Каталог</a></li> :
+                <li className="nav__item"><Link to={AppRoute.CATALOG}  className="nav__link">Каталог</Link></li>
+            }
             <li className="nav__item"> <a className="nav__link" href="#">Где купить?</a></li>
             <li className="nav__item"> <a className="nav__link" href="#">О компании</a></li>
             <li className="nav__item"> <a className="nav__link" href="#">Сервис-центры</a></li>
--- src/components/user-nav/user-nav.jsx
+++ src/components/user-nav/user-nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import Icons  from "../icons/icons";
 import {useSelector} from 'react-redux';
 import {AppRoute} from "../../const";
 
 const UserNav = () => {
+    const location = useLocation();
     const {products} = useSelector((state) => state.CART);
 
     return <div className="header__nav-user nav-user">
@@ -19,12 +20,20 @@ const UserNav = () => {
                     <Icons name="icon-search" height="14" width="14"/>
                 </a>
             </li>
-            <li className="nav-user__item">
-                <Link to={AppRoute.CART} className="nav-user__link" aria-label="Корзина">
-                    <Icons name="icon-basket" height="18" width="16"/>
-                    <span className="nav-user__index">{products.length}</span>
-                </Link>
-            </li>
+            {AppRoute.CART === location.pathname ?
+                <li className="nav-user__item">
+                    <a className="nav-user__link" aria-label="Корзина">
+                        <Icons name="icon-basket" height="18" width="16"/>
+                        <span className="nav-user__index">{products.length}</span>
+                    </a>
+                </li>:
+                <li className="nav-user__item">
+                    <Link to={AppRoute.CART} className="nav-user__link" aria-label="Корзина">
+                        <Icons name="icon-basket" height="18" width="16"/>
+                        <span className="nav-user__index">{products.length}</span>
+                    </Link>
+                </li>
+            }
         </ul>
     </div>
 }
